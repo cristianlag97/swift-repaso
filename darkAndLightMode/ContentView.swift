2422c9d011af50165ba77ccaf21bef93b1b2800c
Cristian Laguna <cristiandavidlaguna.97@gmail.com>
2024-05-04 10:58:51 -0500

    Initial Commit

---
+++ darkAndLightMode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  darkAndLightMode
+//
+//  Created by Cristian David Laguna Aldana on 4/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ darkAndLightMode/darkAndLightModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  darkAndLightModeApp.swift
+//  darkAndLightMode
+//
+//  Created by Cristian David Laguna Aldana on 4/05/24.
+//
+
+import SwiftUI
+
+@main
+struct darkAndLightModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
